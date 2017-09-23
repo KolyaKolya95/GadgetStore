6a8598d1826a96ed7fa582c4c8ea69ec44d49358
KolyaKolya95 <kolya.kolya1495@gmail.com>
2017-09-24 00:34:25 +0300

    Add web API

---
+++ WebUI/Controllers/ListApiController.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebUI.Controllers
+{
+    public class ListApiController : ApiController
+    {
+        private IGadgetRepository repository;
+
+        public ListApiController(IGadgetRepository repo)
+        {
+
+            repository = repo;
+        }
+
+        [HttpGet]
+        [Route("api/list/{id}")]
+        public string GetHello(int id)
+        {
+            return "Hello word " + id;
+        }
+
+        [HttpPost]
+        [Route("api/list/add")]
+        public Gadget Create([FromBody]Gadget gadgetPost)
+        {
+            Gadget gadget = new Gadget();
+           
+
+            if (ModelState.IsValid)
+            {
+                gadget.Name = gadgetPost.Name;
+                gadget.Type = gadgetPost.Type;
+                repository.AddGadget(gadget);
+                return gadget;
+            }
+            else
+            {
+                return gadget;
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/list/{id}")]
+        public string Delete(int id)
+        {
+            repository.DeleteGadgetAPI(id); 
+
+            return "Gadget delete "  + id;
+        }
+    }
+}
---
+++ WebUI/Controllers/SerApiController.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebUI.Controllers
+{
+    public class SerApiController : ApiController
+    {
+
+        private IGadgetRepository repository;
+        
+
+        public SerApiController(IGadgetRepository repo)
+        {
+
+            repository = repo;
+        }
+
+        [HttpGet]
+        [Route("api/smart/hello")]
+        public string Get()
+        {
+            return "Hello world " + DateTime.Now.ToShortDateString();
+        }
+
+
+        [HttpDelete]
+        [Route("api/smart/{id})")]
+        public string DeleteGadgetApi(int gadgetId)
+        {
+            Gadget gadget = repository.Gadgets.FirstOrDefault(b => b.GadgetId == gadgetId);
+            if (ModelState.IsValid)
+            {
+                repository.DeleteGadget(gadget);
+                return "Delete";
+            }
+            else
+            {
+                return "Not found Id gadget";
+            }
+
+        }
+
+        [HttpPost]
+        [Route("api/smart/add")]
+        public string Create([FromBody]Gadget  gadgetpost)
+        {
+            Gadget gadget = new Gadget();
+
+            if (ModelState.IsValid)
+            {
+                gadget.Name = gadgetpost.Name;
+                gadget.Type = gadgetpost.Type;
+                repository.AddGadget(gadget);
+               
+                return "Add to : " + gadget.Name;
+            }
+            else
+            {
+                return "error";
+            }
+        }
+    }
+}
